d30277b799ef72030310de8d7489fc56a1178e82
venkatramana <ovenkatramana01@gmail.com>
2025-09-09 12:51:15 +0530

    Delete load_data.py

--- load_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-import numpy as np
-
-@st.cache_data
-def load_data(file_path="D:\venkatesh python class\Project\application_train (1).csv"):
-    df = pd.read_csv(file_path)
-    return df
-
